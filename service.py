96aa7dd177ef079f9a574950df14d86afdc91bd1
ugurnuraydin <ugur.nuraydin@gmail.com>
2024-03-27 22:05:07 +0300

    Berkcana Özel Geliştirmeler Eklendi, Belçika özelinde geliştirmeler eklendi

--- service.py
+++ service.py
@@ -12,12 +12,14 @@ import logging
 TELEGRAM_BOT_TOKEN = '6829298339:AAEJjoijTUf8DyaERraNevg9S9jHIRosMhg'
 TELEGRAM_GROUP_CHAT_ID = '-1002051631354' #Canlı Grup
 TELEGRAM_KISISEL_CHAT_ID = '1418776096' #Kişisel Chat
+TELEGRAM_BERKCAN_CHAT_ID = '-4188174746' #Berkcan Chat
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 UTC_PLUS_3 = timezone(timedelta(hours=3))
 now = datetime.now(UTC_PLUS_3)
 GROUP = "GROUP"
 PERSONAL = "PERSONAL"
+BERKCAN = "BERKCAN"
 
 
 def save_last_message(data):
@@ -31,14 +33,19 @@ def load_last_message():
     except FileNotFoundError:
         return None
 
-async def send_telegram_message(text,chatType):
-    if chatType == GROUP:
-        await bot.send_message(chat_id=TELEGRAM_GROUP_CHAT_ID, text=text, parse_mode='Markdown')
-        logging.info('Gruba Mesaj Gönderildi.')
+async def send_telegram_message(text,chatType = "REST"):
+    if chatType == PERSONAL: 
+        await bot.send_message(chat_id=TELEGRAM_KISISEL_CHAT_ID, text=text, parse_mode='Markdown')
+        logging.info('Kişisel Mesaj Gönderildi.')
     else:
-       await bot.send_message(chat_id=TELEGRAM_KISISEL_CHAT_ID, text=text, parse_mode='Markdown') 
-       logging.info('Kişisel Mesaj Gönderildi.')
-
+        if time(9, 0) <= now.time() <= time(23, 0) and now.minute % 15 == 0:
+            await bot.send_message(chat_id=TELEGRAM_GROUP_CHAT_ID, text=text, parse_mode='Markdown') 
+            logging.info('Gruba Mesaj Gönderildi.')
+            
+        await bot.send_message(chat_id=TELEGRAM_BERKCAN_CHAT_ID, text=text, parse_mode='Markdown') 
+        logging.info('Berkcana Mesaj Gönderildi.')
+        
+        
 async def fetch_visa_appointments(now):
     logging.info('Randevu Kontrol Akışı Başladı --' + now.strftime("%H:%M:%S"))
     global lastMessage
@@ -73,6 +80,15 @@ async def fetch_visa_appointments(now):
         
     sorted_list = sorted(sonucListe, key=lambda x: x["mission_country"])
     
+    
+    for item in sorted_list:
+        if item["mission_country"] == "Belgium" and item["appointment_date"] is not None:
+            message_content = f"Belgium için randevu bulundu!! \nRandevu Merkezi: {item['center_name']}, \nRandevu Tarihi: {item['appointment_date']}"
+            logging.info('Belgium için randevu bulundu:')
+            await send_telegram_message(message_content, PERSONAL)
+            break
+    
+
     messages = []
 
     for mission_country, items in groupby(sorted_list, key=lambda x: x["mission_country"]):
@@ -91,7 +107,7 @@ async def fetch_visa_appointments(now):
         final_message = "\n\n".join(messages)
         logging.info('Mesaj Farkı Bulundu, Son Mesaj:\n' + final_message)
         if sonucListe and messages:
-            await send_telegram_message(final_message,GROUP)
+            await send_telegram_message(final_message)
             return None
     else:
         logging.info('Randevularda Değişiklik yok, mesaj gönderilmedi')
@@ -104,14 +120,14 @@ async def scheduler():
     while True:
         sleep_seconds = None
         now = datetime.now(UTC_PLUS_3) 
-        if time(9, 0) <= now.time() <= time(20, 0):
+        if time(6, 0) <= now.time() <= time(23, 0):
             await fetch_visa_appointments(now)
-            next_run = now + timedelta(minutes=15)
+            next_run = now + timedelta(minutes=5)
             sleep_seconds = (next_run - now).total_seconds()
             logging.info("Sonraki Çalışma: " + next_run.strftime("%H:%M:%S"))
         else:
             tomorrow = now + timedelta(days=1)
-            next_run = tomorrow.replace(hour=9, minute=0, second=0, microsecond=0)
+            next_run = tomorrow.replace(hour=6, minute=0, second=0, microsecond=0)
             sleep_seconds = (next_run - now).total_seconds()
             logging.info("Servis Duraklatıldı, Sonraki Çalışma Yarın: " + next_run.strftime("%H:%M:%S"))
         logging.info('___________________________________________________________________\n')
