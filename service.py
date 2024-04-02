6b5c9a5d5e225dbcb543ad49c614a61aea83e4af
ugurnuraydin <ugur.nuraydin@gmail.com>
2024-04-02 10:19:50 +0300

    Kod İyileştirmeleri

--- service.py
+++ service.py
@@ -8,167 +8,116 @@ import json
 import logging
 
 
-# Telegram bot token ve grup ID bilgilerinizi buraya girin
-TELEGRAM_BOT_TOKEN = '6829298339:AAEJjoijTUf8DyaERraNevg9S9jHIRosMhg'
-TELEGRAM_GROUP_CHAT_ID = '-1002051631354' #Canlı Grup
-TELEGRAM_KISISEL_CHAT_ID = '1418776096' #Kişisel Chat
-TELEGRAM_BERKCAN_CHAT_ID = '-4188174746' #Berkcan Chat
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
+# Yapılandırma
+config = {
+    "telegram_bot_token": '6829298339:AAEJjoijTUf8DyaERraNevg9S9jHIRosMhg',
+    "telegram_group_chat_id": '-1002051631354',
+    "telegram_personal_chat_id": '1418776096',
+    "telegram_berkcan_chat_id": '-4188174746',
+    "utc_plus_3": timezone(timedelta(hours=3))
+}
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-UTC_PLUS_3 = timezone(timedelta(hours=3))
+bot = Bot(token=config["telegram_bot_token"])
 
 
-def save_last_message(data):
-    with open("lastMessage.json", "w") as file:
-        json.dump(data, file)
-        
-def load_last_message():
+def load_json(filename):
     try:
-        with open("lastMessage.json", "r") as file:
+        with open(filename, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return None
 
-def save_last_message_berkcan(data):
-    with open("lastMessageBerkcan.json", "w") as file:
+def save_json(filename, data):
+    with open(filename, "w") as file:
         json.dump(data, file)
-        
-def load_last_message_berkcan():
-    try:
-        with open("lastMessageBerkcan.json", "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return None
 
-async def send_telegram_message_general(text, now):
-    if time(9, 0) <= now.time() <= time(21, 0):
-        await bot.send_message(chat_id=TELEGRAM_GROUP_CHAT_ID, text=text, parse_mode='Markdown')
-        logging.info('Gruba Mesaj Gönderildi.' )
-            
-async def send_telegram_message_berkcan(text):
-    await bot.send_message(chat_id=TELEGRAM_BERKCAN_CHAT_ID, text=text, parse_mode='Markdown') 
-    logging.info('Berkcana Mesaj Gönderildi.')
+async def send_message(chat_id, text, now):
+    if chat_id == config["telegram_group_chat_id"] and time(9, 0) <= now.time() <= time(21, 0):
+        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+        logging.info(f'Message sent to general group at {now.strftime("%H:%M:%S")}')
+    elif chat_id == config["telegram_berkcan_chat_id"]:
+        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+        logging.info(f'Message sent to Berkcan at {now.strftime("%H:%M:%S")}')
+    elif chat_id == config["telegram_personal_chat_id"]:
+        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+        logging.info(f'Message sent to Uğur at {now.strftime("%H:%M:%S")}')
+
         
-            
-async def send_telegram_message_personal(text):
-    await bot.send_message(chat_id=TELEGRAM_KISISEL_CHAT_ID, text=text, parse_mode='Markdown')
-    logging.info('Kişisel Mesaj Gönderildi.')
-        
-        
-async def fetch_visa_appointments(now):
-    logging.info('Randevu Kontrol Akışı Başladı --' + now.strftime("%H:%M:%S"))
-    global lastMessage
-    global lastMessageBerkcan
+async def fetch_and_notify(now):
+    logging.info('Checking appointments --' + now.strftime("%H:%M:%S"))
     url = "https://api.schengenvisaappointments.com/api/visa-list/"
     
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            logging.info('Randevu Bilgileri Alındı')
+        response.raise_for_status()
+        data = response.json()
+        logging.info('Appointment data retrieved')
+    except requests.RequestException as e:
+        logging.error(f"Error fetching data: {e}")
+        await send_message(config["telegram_personal_chat_id"], f"Error fetching data: {e}", now)
+        return
+
+    await process_appointments(data, "general", now)
+    await process_appointments(data, "berkcan", now)
+    await process_appointments(data, "personal", now)
+    
+async def process_appointments(data, category, now):
+    filtered_data = filter_appointments(data, category)
+    last_messages = load_json(f"lastMessage{category.capitalize()}.json")
+    final_message = "\n\n".join(format_message(item,category) for item in filtered_data)
+    if category != "personal":  
+        if not final_message == last_messages:
+            save_json(f"lastMessage{category.capitalize()}.json", final_message)
+            await notify_users(category, final_message, now)
         else:
-            logging.error(f"API'den beklenen yanıt alınamadı, durum kodu: {response.status_code}")
-            await send_telegram_message_personal(f"API'den beklenen yanıt alınamadı, durum kodu:\n {response.status_code}")
-            return None
-    except requests.exceptions.RequestException as e:
-        logging.error(f"API'den hata alındı, durum kodu: {response.status_code}")
-        await send_telegram_message_personal(f"API isteği sırasında bir hata oluştu:\n {e}")
-        return None
-
-    genel_sonucListe = [
-        item for item in data if item["source_country"] == "Turkiye" and 
-        # (item["mission_country"] == "Belgium" or item["mission_country"] == "Netherlands") and
-        # item["visa_category"] == "KISA DONEM BASVURU / SHORT TERM APPLICATION" and 
-        "tourism" in item["visa_subcategory"].lower() and
-        item["appointment_date"] is not None
-    ]
-    
-    berkcan_genel_sonucListe = [
-    item for item in data if item["source_country"] == "Turkiye" and 
-    item["mission_country"] in ["France", "Czechia", "Austria", "Poland","Belgium","Netherlands"] and
-    item["appointment_date"] is not None
-]
-
-    
-
-        
-    berkcan_sorted_list = sorted(berkcan_genel_sonucListe, key=lambda x: x["mission_country"])
-    general_sorted_list = sorted(genel_sonucListe, key=lambda x: x["mission_country"])
-    
-    
-    for item in general_sorted_list:
-        if item["mission_country"] == "Belgium" and item["appointment_date"] is not None:
-            message_content = f"Belgium için randevu bulundu!! \nRandevu Merkezi: {item['center_name']}, \nRandevu Tarihi: {item['appointment_date']}"
-            logging.info('Belgium için randevu bulundu:')
-            await send_telegram_message_personal(message_content)
-            break
-    
-
-    general_messages = []
-    berkcan_messages = []
-    lastMessage = load_last_message()
-    lastMessageBerkcan = load_last_message_berkcan()
-
-
-    for mission_country, items in groupby(general_sorted_list, key=lambda x: x["mission_country"]):
-        for item in items:
-            if item['appointment_date'] is not None:
-                appointment_date = datetime.fromisoformat(item['appointment_date']).strftime('%d-%m-%Y')
-            else:
-                appointment_date = "Uyumsuz Format"        
-            message = f"**Ülke:** {mission_country}, \n**Randevu Merkezi:** {item['center_name']}, \n**Randevu Tarihi:** {appointment_date}"
-        
-            general_messages.append(message)
-            
-
-    for mission_country, items in groupby(berkcan_sorted_list, key=lambda x: x["mission_country"]):
-        for item in items:
-            if item['appointment_date'] is not None:
-                appointment_date = datetime.fromisoformat(item['appointment_date']).strftime('%d-%m-%Y')
-            else:
-                appointment_date = "Uyumsuz Format"        
-            message = f"**Ülke:** {mission_country}, \n**Randevu Merkezi:** {item['center_name']}, \n**Vize Türü:** {item['visa_category']}, \n**Randevu Tarihi:** {appointment_date}"
-        
-            berkcan_messages.append(message)
-            
-        
-    
-    if  general_messages != lastMessage:
-        lastMessage = general_messages
-        save_last_message(general_messages)
-        final_message = "\n\n".join(general_messages)
-        # logging.info('Mesaj Farkı Bulundu, Son Mesaj:\n' + final_message)
-        await send_telegram_message_general(final_message, now)
-        
+            logging.info(f'No changes in appointments for {category}')
     else:
-        logging.info('Randevularda Değişiklik yok, mesaj gönderilmedi')
-
-    if  berkcan_messages != lastMessageBerkcan:
-        lastMessageBerkcan = berkcan_messages
-        save_last_message_berkcan(general_messages)
-        final_message = "\n\n".join(berkcan_messages)
-        # logging.info('Mesaj Farkı Bulundu, Son Mesaj:\n' + final_message)
-        await send_telegram_message_berkcan(final_message)
+        #Turistik belçika için özel kontrol eklendi.
+        if filtered_data: 
+            await send_message(config["telegram_personal_chat_id"], final_message, now)
         
-    else:
-        logging.info('Randevularda Değişiklik yok, mesaj gönderilmedi')
+        
+def filter_appointments(data, category):
+    if category == "general":
+        return [item for item in data if item["source_country"] == "Turkiye" and "tourism" in item["visa_subcategory"].lower() and item["appointment_date"] is not None]
+    elif category == "berkcan":
+        return [item for item in data if item["source_country"] == "Turkiye" and item["mission_country"] in ["France", "Czechia", "Austria", "Poland", "Belgium", "Netherlands"] and item["appointment_date"] is not None]
+    elif category == "personal":
+        return [item for item in data if item["source_country"] == "Turkiye" and item["mission_country"] == "Belgium" and "tourism" in item["visa_subcategory"].lower() and item["appointment_date"] is not None]
+
+
+def format_message(item, category):
+    appointment_date = datetime.fromisoformat(item['appointment_date']).strftime('%d-%m-%Y')
+    if category == "general":
+        return f"<b><u>Ülke:</u></b> {item['mission_country']},\n<b><u>Merkez:</u></b> {item['center_name']},\n<b><u>Date:</u></b> {appointment_date}"
+
+    elif category == "berkcan":
+        return f"<u><b>Ülke:</b></u> {item['mission_country']},\n<u><b>Kategori:</b></u> {item['visa_category']},\n<u><b>Tip:</b></u> {item['visa_subcategory']},\n<u><b>Merkez:</b></u> {item['center_name']},\n<u><b>Date:</b></u> {appointment_date}"
     
+    if category == "personal":
+        return f"<b><u>BELÇİKA İÇİN RANDEVU BULUNDU!!</u></b>\n<b><u>Ülke:</u></b> {item['mission_country']}, <b><u>Merkez:</u></b> {item['center_name']}, <b><u>Date:</u></b> {appointment_date}"
+
+async def notify_users(category, message, now):
+    chat_ids = {
+        "general": [config["telegram_group_chat_id"]],
+        "berkcan": [config["telegram_berkcan_chat_id"]]
+    }
+    for chat_id in chat_ids.get(category, []):
+        await send_message(chat_id, message, now)
+
 
 async def scheduler():
-     while True:
-        now = datetime.now(UTC_PLUS_3)  # Şu anki zamanı UTC+3 olarak al
-        await fetch_visa_appointments(now)  # Şu anki zamanı fonksiyona argüman olarak gönder
-        next_run = now + timedelta(minutes=10)  # Bir sonraki çalışma için 10 dakika sonrasını hesapla
-        sleep_seconds = (next_run - datetime.now(UTC_PLUS_3)).total_seconds()  # Bekleme süresini hesapla
-
+    while True:
+        now = datetime.now(config["utc_plus_3"])
+        await fetch_and_notify(now)
+        next_run = now + timedelta(minutes=10)
+        sleep_seconds = (next_run - datetime.now(config["utc_plus_3"])).total_seconds()
         logging.info("Sonraki Çalışma: " + next_run.strftime("%H:%M:%S"))
         logging.info('___________________________________________________________________\n')
 
         await asyncio.sleep(sleep_seconds)  # Belirlenen süre kadar bekle
         
-
-
-
 async def main():
     logging.info('VisaBot Aktif')
     await scheduler()
