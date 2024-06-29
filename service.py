4297da3e33b105785131556d859fc0c9bc816ba0
ugurnuraydin <ugur.nuraydin@gmail.com>
2024-06-29 19:36:09 +0300

    Ülkere Göre Ayrım Yapıldı

--- service.py
+++ service.py
@@ -11,42 +11,39 @@ import logging
 # Yapılandırma
 config = {
     "telegram_bot_token": '6829298339:AAEJjoijTUf8DyaERraNevg9S9jHIRosMhg',
+    
     "telegram_group_chat_id": '-1002051631354',
     "telegram_personal_chat_id": '1418776096',
-    "telegram_berkcan_chat_id": '-4188174746',
+    
+    "telegram_czech_chat_id": '-4253313815',
+    "telegram_belgium_chat_id": '-4238889616',
+    "telegram_france_chat_id": '-4267407232',
+    "telegram_netherlands_chat_id": '-4277752437',
+    "telegram_slovenia_chat_id": '-4176399641',
     "utc_plus_3": timezone(timedelta(hours=3))
 }
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 bot = Bot(token=config["telegram_bot_token"])
 
-
-def load_json(filename):
-    try:
-        with open(filename, "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return None
-
-def save_json(filename, data):
-    with open(filename, "w") as file:
-        json.dump(data, file)
+def reset_lists():
+    global czechia_list, belgium_list, france_list, netherlands_list, slovenia_list
+    czechia_list = []
+    belgium_list = []
+    france_list = []
+    netherlands_list = []
+    slovenia_list = []
 
 async def send_message(chat_id, text, now):
-    if chat_id == config["telegram_group_chat_id"] and time(9, 0) <= now.time() <= time(21, 0):
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
-        logging.info(f'Message sent to general group at {now.strftime("%H:%M:%S")}')
-    elif chat_id == config["telegram_berkcan_chat_id"]:
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
-        logging.info(f'Message sent to Berkcan at {now.strftime("%H:%M:%S")}')
-    elif chat_id == config["telegram_personal_chat_id"]:
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
-        logging.info(f'Message sent to Uğur at {now.strftime("%H:%M:%S")}')
+    await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
+    logging.info(f'Message sent to general group at {now.strftime("%H:%M:%S")}')
 
         
 async def fetch_and_notify(now):
     logging.info('Checking appointments --' + now.strftime("%H:%M:%S"))
     url = "https://api.schengenvisaappointments.com/api/visa-list/"
+    
+    reset_lists()
     
     try:
         response = requests.get(url)
@@ -57,55 +54,54 @@ async def fetch_and_notify(now):
         logging.error(f"Error fetching data: {e}")
         await send_message(config["telegram_personal_chat_id"], f"Error fetching data: {e}", now)
         return
-
-    await process_appointments(data, "general", now)
-    await process_appointments(data, "berkcan", now)
-    await process_appointments(data, "personal", now)
     
-async def process_appointments(data, category, now):
-    filtered_data = filter_appointments(data, category)
-    last_messages = load_json(f"lastMessage{category.capitalize()}.json")
-    final_message = "\n\n".join(format_message(item,category) for item in filtered_data)
-    if category != "personal":  
-        if not final_message == last_messages:
-            save_json(f"lastMessage{category.capitalize()}.json", final_message)
-            await notify_users(category, final_message, now)
-        else:
-            logging.info(f'No changes in appointments for {category}')
-    else:
-        #Turistik belçika için özel kontrol eklendi.
-        if filtered_data: 
-            await send_message(config["telegram_personal_chat_id"], final_message, now)
+    await process_appointments(data, now)
+
+    
+async def process_appointments(data,now):
+    filter_appointments(data)
+    final_message_czechia = "\n\n".join(format_message(item) for item in czechia_list)
+    final_message_belgium = "\n\n".join(format_message(item) for item in belgium_list)
+    final_message_france = "\n\n".join(format_message(item) for item in france_list)
+    final_message_netherlands = "\n\n".join(format_message(item) for item in netherlands_list)
+    final_message_slovenia = "\n\n".join(format_message(item) for item in slovenia_list)
+    
+    await notify_users(final_message_czechia, final_message_belgium, final_message_france, final_message_netherlands, final_message_slovenia, now)
+
         
         
-def filter_appointments(data, category):
-    if category == "general":
-        return [item for item in data if item["source_country"] == "Turkiye" and "tourism" in item["visa_subcategory"].lower() and item["appointment_date"] is not None]
-    elif category == "berkcan":
-        return [item for item in data if item["source_country"] == "Turkiye" and item["mission_country"] in ["France", "Czechia", "Austria", "Poland", "Belgium", "Netherlands"] and item["appointment_date"] is not None]
-    elif category == "personal":
-        # return [item for item in data if item["source_country"] == "Turkiye" and (item["mission_country"] == "Belgium" or "ankara" in item['center_name'].lower()) and "tourism" in item["visa_subcategory"].lower() and item["appointment_date"] is not None]
-        return [item for item in data if item["source_country"] == "Turkiye" and item["mission_country"] == "Belgium" and "tourism" in item["visa_subcategory"].lower() and item["appointment_date"] is not None]
+def filter_appointments(data):
+    for item in data:
+        if item["source_country"] == "Turkiye" and item["appointment_date"] is not None:
+            visa_subcategory = item.get("visa_subcategory") or ""
+            if item["mission_country"] == "Czechia" and "tourism" in visa_subcategory.lower():
+                czechia_list.append(item)
+            elif item["mission_country"] == "Belgium" and "tourism" in visa_subcategory.lower():
+                belgium_list.append(item)
+            elif item["mission_country"] == "France" and (visa_subcategory == "Short Term Standard" or visa_subcategory == "Tourism/Family visit"):
+                france_list.append(item)
+            elif item["mission_country"] == "Netherlands" and "tourism" in visa_subcategory.lower():
+                netherlands_list.append(item)
+            elif item["mission_country"] == "Slovenia" and "tourism" in visa_subcategory.lower():
+                slovenia_list.append(item)
 
 
-def format_message(item, category):
+
+def format_message(item):
     appointment_date = datetime.fromisoformat(item['appointment_date']).strftime('%d-%m-%Y')
-    if category == "general":
-        return f"<b><u>Ülke:</u></b> {item['mission_country']},\n<b><u>Merkez:</u></b> {item['center_name']},\n<b><u>Date:</u></b> {appointment_date}"
+    return f"<u><b>Ülke:</b></u> {item['mission_country']},\n<u><b>Kategori:</b></u> {item['visa_category']},\n<u><b>Tip:</b></u> {item['visa_subcategory']},\n<u><b>Merkez:</b></u> {item['center_name']},\n<u><b>Date:</b></u> {appointment_date}"
 
-    elif category == "berkcan":
-        return f"<u><b>Ülke:</b></u> {item['mission_country']},\n<u><b>Kategori:</b></u> {item['visa_category']},\n<u><b>Tip:</b></u> {item['visa_subcategory']},\n<u><b>Merkez:</b></u> {item['center_name']},\n<u><b>Date:</b></u> {appointment_date}"
-    
-    if category == "personal":
-        return f"<b><u>BELÇİKA İÇİN RANDEVU BULUNDU!!</u></b>\n<b><u>Ülke:</u></b> {item['mission_country']},\n<b><u>Merkez:</u></b> {item['center_name']},\n<b><u>Date:</u></b> {appointment_date}\n\nhttps://visa.vfsglobal.com/tur/tr/bel/login"
-
-async def notify_users(category, message, now):
-    chat_ids = {
-        "general": [config["telegram_group_chat_id"]],
-        "berkcan": [config["telegram_berkcan_chat_id"]]
+async def notify_users(czechia_message, belgium_message, france_message, netherlands_message, slovenia_message, now):
+    chat_messages = {
+        config["telegram_czech_chat_id"]: czechia_message,
+        config["telegram_belgium_chat_id"]: belgium_message,
+        config["telegram_france_chat_id"]: france_message,
+        config["telegram_netherlands_chat_id"]: netherlands_message,
+        config["telegram_slovenia_chat_id"]: slovenia_message
     }
-    for chat_id in chat_ids.get(category, []):
-        await send_message(chat_id, message, now)
+    for chat_id, message in chat_messages.items():
+        if message:  # Eğer mesaj boş değilse gönder
+            await send_message(chat_id, message, now)
 
 
 async def scheduler():
