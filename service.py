df4a97ed4b9bc83122fb39bd2619e9af9f60db29
ugurnuraydin <ugur.nuraydin@gmail.com>
2024-12-28 23:50:06 +0300

    Filtre düzenlemeleri ve performans iyileştirmeleri

--- service.py
+++ service.py
@@ -24,7 +24,7 @@ config = {
     "telegram_slovenia_chat_id": os.getenv('TELEGRAM_SLOVENIA_CHAT_ID'),
     "telegram_fransa_sener_chat_id": os.getenv('TELEGRAM_FRANSA_SENER_CHAT_ID'),
     "telegram_dumbs_chat_id": os.getenv('TELEGRAM_DUMBS_CHAT_ID'),
-    "visa_api_url" : os.getenv('VISA_API_URL'),
+    "visa_api_url": os.getenv('VISA_API_URL'),
     "chat_names": {
         os.getenv('TELEGRAM_GROUP_CHAT_ID'): "Genel Grup",
         os.getenv('TELEGRAM_PERSONAL_CHAT_ID'): "Kişisel Chat",
@@ -37,9 +37,7 @@ config = {
         os.getenv('TELEGRAM_DUMBS_CHAT_ID'): "Dumbs"
     },
     "utc_plus_3": timezone(timedelta(hours=3))
-    
 }
-
 
 # Global last_message_files tanımı
 last_message_files = {
@@ -72,47 +70,57 @@ def save_json(filename, data):
     with open(filename, "w") as file:
         json.dump(data, file)
 
+def get_chat_name(chat_id):
+    return config["chat_names"].get(chat_id, "Bilinmeyen Grup")
+
+def log_message(message, chat_id):
+    chat_name = get_chat_name(chat_id)
+    logging.info(f"{message} - Grup: {chat_name}, Tarih: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
 async def send_message(chat_id, text, now):
-    chat_name = config["chat_names"].get(chat_id, chat_id)  # chat_id için grup ismini al, bulunamazsa chat_id'yi kullan
+    chat_name = get_chat_name(chat_id)
+
+    if not text.strip():  # Mesaj boşsa logla ve gönderme
+        log_message("Mesaj boş olduğu için gönderilmiyor", chat_id)
+        return
 
     if chat_id in last_message_files:
         # last_message_files içindeki id'ler için sadece 08:00-23:00 arasında mesaj gönder
         if time(8, 0) <= now.time() <= time(23, 0):
             await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
-            logging.info(f'Message sent to {chat_name} at {now.strftime("%H:%M:%S")}')
+            log_message("Mesaj gönderildi", chat_id)
         else:
-            logging.info(f'Message to {chat_name} not sent due to time restrictions.')
+            log_message("Mesaj zaman kısıtlaması nedeniyle gönderilmiyor", chat_id)
     else:
         # Diğer id'ler için saat kısıtlaması olmadan mesaj gönder
         await bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
-        logging.info(f'Message sent to {chat_name} at {now.strftime("%H:%M:%S")}')
+        log_message("Mesaj gönderildi", chat_id)
 
 async def fetch_and_notify(now):
-    logging.info('Checking appointments --' + now.strftime("%H:%M:%S"))
+    log_message("Randevular kontrol ediliyor", config["telegram_personal_chat_id"])
     url = config["visa_api_url"]
-    
+
     reset_lists()
-    
+
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        
+
         # Boş veri kontrolü
         if not data:  # Eğer data None, boş liste veya boş dictionary ise
-            logging.warning('API response is empty or null')
-            await send_message(config["telegram_personal_chat_id"], f"API'den dönen veri boş veya null: {url}", now )
+            log_message("API yanıtı boş veya null", config["telegram_personal_chat_id"])
+            await send_message(config["telegram_personal_chat_id"], f"API'den dönen veri boş veya null: {url}", now)
             return  # Eğer veri boşsa, devam etmeyi durdur
-        
-        logging.info('Appointment data retrieved')
+
+        log_message("Randevu verileri alındı", config["telegram_personal_chat_id"])
     except requests.RequestException as e:
-        logging.error(f"Error fetching data: {e}")
-        await send_message(config["telegram_personal_chat_id"], f"Error fetching data: {e}", now)
+        log_message(f"Veri çekme hatası: {e}", config["telegram_personal_chat_id"])
+        await send_message(config["telegram_personal_chat_id"], f"Veri çekme hatası: {e}", now)
         return
-    
+
     await process_appointments(data, now)
 
-    
 async def process_appointments(data, now):
     filter_appointments(data)
     final_message_czechia = "\n\n".join(format_message(item) for item in czechia_list)
@@ -123,7 +131,7 @@ async def process_appointments(data, now):
     final_message_slovenia = "\n\n".join(format_message(item) for item in slovenia_list)
     final_message_dumbs = "\n\n".join(format_message(item) for item in dumbs_list)
     final_message_general = "\n\n".join(format_message(item) for item in general_list)
-    
+
     await notify_users(
         final_message_czechia, 
         final_message_belgium, 
@@ -137,32 +145,54 @@ async def process_appointments(data, now):
     )
 
 def filter_appointments(data):
-    for item in data:
-        if item["source_country"] == "Turkiye" and item["appointment_date"] is not None:
-            visa_subcategory = item.get("visa_subcategory") or ""
-            
-            if item["mission_country"] == "Czechia" and "tourism" in visa_subcategory.lower():
-                czechia_list.append(item)
-                
-            if item["mission_country"] == "Belgium" and "tourism" in visa_subcategory.lower():
-                belgium_list.append(item)
-                
-            if item["mission_country"] == "France" and (visa_subcategory == "Short Term Standard" or visa_subcategory == "Tourism/Family visit"):
-                france_list.append(item)
-                france_sener_list.append(item)
-                
-            if item["mission_country"] == "Netherlands" and "tourism" in visa_subcategory.lower():
-                netherlands_list.append(item)
-                
-            if item["mission_country"] == "Slovenia" and "tourism" in visa_subcategory.lower():
-                slovenia_list.append(item)
-                
-            if (item["mission_country"] in ["Netherlands", "Belgium"]) and ("ankara" in item["center_name"].lower() or "istanbul" in item["center_name"].lower()) and "tourism" in visa_subcategory.lower():
-                dumbs_list.append(item)
-                
-            if ("ankara" in item["center_name"].lower() or "istanbul" in item["center_name"].lower() or "izmir" in item["center_name"].lower()) and ("tourism" in visa_subcategory.lower() or "short term standard" in visa_subcategory.lower()):
-                general_list.append(item)
     
+    dataForTurkey = [
+        item for item in data
+        if item.get("source_country") == "Turkiye"
+        and item.get("appointment_date") is not None
+        and "tourism" in (item.get("visa_subcategory") or "").lower()
+    ]
+
+    for item in dataForTurkey:
+    
+        center_name = item["center_name"].lower()  # Küçük harfe çevir
+        mission_country = item["mission_country"].lower()  # Küçük harfe çevir
+
+        # Czechia
+        if mission_country == "czechia":
+            czechia_list.append(item)
+
+        # Belgium
+        if mission_country == "belgium":
+            belgium_list.append(item)
+
+        # France
+        if mission_country == "france":
+            france_list.append(item)
+            # france_sener_list.append(item)
+
+        # Netherlands
+        if mission_country == "netherlands":
+            netherlands_list.append(item)
+
+        # Slovenia
+        if mission_country == "slovenia":
+            slovenia_list.append(item)
+
+        # Dumbs List
+        if mission_country in {"netherlands", "belgium", "france"} and (
+            "ankara" in center_name or "istanbul" in center_name
+        ):
+            dumbs_list.append(item)
+
+        # General List
+        if (
+            "ankara" in center_name
+            or "istanbul" in center_name
+        ):
+            general_list.append(item)
+
+
     # general_list'i item["mission_country"]'ye göre alfabetik olarak sıralayın
     general_list.sort(key=lambda x: x["mission_country"])
 
@@ -191,14 +221,20 @@ async def notify_users(
         config["telegram_dumbs_chat_id"]: dumbs_message,
         config["telegram_group_chat_id"]: general_message
     }
-    
+
     for chat_id, message in chat_messages.items():
+        if not message or not message.strip():  # Mesaj boşsa atla ve logla
+            log_message("Mesaj boş. Gönderim atlandı.", chat_id)
+            continue
+
         if chat_id in last_message_files:
             last_message = load_json(last_message_files[chat_id])
             if last_message != message:
                 await send_message(chat_id, message, now)
                 save_json(last_message_files[chat_id], message)
-        elif message:  # Eğer mesaj boş değilse gönder
+            else:
+                log_message("Mesajda değişiklik yok. Gönderim atlandı.", chat_id)
+        else:  # Eğer chat_id `last_message_files`'ta değilse ve mesaj boş değilse gönder
             await send_message(chat_id, message, now)
 
 async def scheduler():
@@ -207,12 +243,12 @@ async def scheduler():
         await fetch_and_notify(now)
         next_run = now + timedelta(minutes=5)
         sleep_seconds = (next_run - datetime.now(config["utc_plus_3"])).total_seconds()
-        logging.info("Sonraki Çalışma: " + next_run.strftime("%H:%M:%S"))
-        logging.info('___________________________________________________________________\n')
+        log_message("Sonraki Çalışma Planlandı", config["telegram_personal_chat_id"])
+        logging.info("_______________________________________________________________")
         await asyncio.sleep(sleep_seconds)  # Belirlenen süre kadar bekle
-        
+
 async def main():
-    logging.info('VisaBot Aktif')
+    log_message("VisaBot Aktif", config["telegram_personal_chat_id"])
     await scheduler()
 
 if __name__ == "__main__":
